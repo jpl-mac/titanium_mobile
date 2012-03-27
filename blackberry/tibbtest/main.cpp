5d4bbe5f94d4d0725fb98ccecb229eed3bff1489
D Campbell <dcampbell@macadamian.com>
2012-03-27 14:43:42 -0400

    Created a native QNX application for testing Titanium runtime

---
+++ blackberry/tibbtest/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2012-03-27
+ *      Author: dcampbell
+ */
+
+#include "tibb.h"
+
+int cascades_user_main(int argc,char **argv)
+{
+	int ret=tibb_run("",argc,argv);
+	return ret;
+}
