1d4f270a7d328e00736fbad40e58144d28ce88a4
unknown <alexg@AlexG.(none)>
2012-04-13 20:02:38 +0400

    bar-descriptor.xml .project templates creation
    Changelog:
     - Delete the bar-descriptor.xml and .project files from HelloDisplayWorld
       temporariry folder.
     - Added functionality to copy the mentioned files during project creation
     - Added functionality to replace template fields after copy
     - Added unit test for testing templating on both files with -t option
    
    TESTS:
     - Run the command "blackberry.py HelloWorldDisplay com.macadamian.helloworlddis   play "c:\Users\alexg\Documents\Titanium Studio Workspace" C:\bbndk-2.0.0
     - Make sure it added the bar-descriptor.xml and .project files in project
       with appropriate replacements
     - Make sure it successfully builds after project creation.
     - Run the command "blackberry.py -t"
     - Make sure tests passed for both files

--- support/blackberry/blackberry.py
+++ support/blackberry/blackberry.py
@@ -22,7 +22,24 @@ class Blackberry(object):
 		self.name = name
 		self.id = appid
 		self.ndk = bbndk
-
+		
+		# Configuration for the bar-descriptor.xml file
+		self.configDescriptor = {
+			'id':self.id,
+			'appname':self.name,
+			'platformversion':self.ndk.version,
+			'description':None,
+			'version':'1.0',
+			'author':'Appcelerator Titanium Mobile', # TODO MAC: Find out how validate the author
+			'category':'core.games'
+		}
+		
+		# Configuration for the project file
+		self.configProject = {
+			'appname':self.name,
+			'buildlocation':None # TODO MAC: Find out how specify the build location
+		}
+		
 	def create(self, dir): 
 		project_dir = os.path.join(dir, self.name)
 
@@ -40,8 +57,8 @@ class Blackberry(object):
 		if os.path.exists(blackberry_project_resources):
 			shutil.rmtree(blackberry_project_resources)
 		shutil.copytree(os.path.join(template_dir,'resources'),blackberry_project_resources)
-		# TODO Mac: For now used temporarily created directory where exist files as:
-		# bar-descriptor.xml, project files and sources.
+		
+		# TODO Mac: For now used temporarily created directory where exist source files
 		sourcePath = os.path.join(template_dir,'HelloWorldDisplay')
 		for file in os.listdir(sourcePath):
 			path = os.path.join(sourcePath, file)
@@ -56,9 +73,17 @@ class Blackberry(object):
 			except Exception, e:
 				print >> sys.stderr, e
 				sys.exit(1)
+				
+		# add replaced templates: bar-descriptor.xml, .project files
+		templates = os.path.join(template_dir,'templates')
+		# copy bar-descriptor.xml
+		shutil.copy2(os.path.join(templates,'bar-descriptor.xml'), build_dir)
+		render(os.path.join(build_dir,'bar-descriptor.xml'), self.configDescriptor)
+		# copy project file
+		shutil.copy2(os.path.join(templates,'project'), os.path.join(build_dir, '.project'))
+		render(os.path.join(build_dir,'.project'), self.configProject)		
 
 		# import project into workspace so it can be built with mkbuild
-		print build_dir
 		self.ndk.importProject(build_dir)
 
 		# TODO Mac: not sure what this is trying to accomplish
@@ -67,7 +92,63 @@ class Blackberry(object):
 		#if not os.path.exists(blackberry_resources_dir):
 		#	os.makedirs(blackberry_resources_dir)
 
+def render(template, config):
+	baseDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+	baseDir = os.path.dirname(baseDir)
+	sys.path.append(os.path.join(baseDir, 'common'))
+	from mako.template import Template
+	tmpl = load_template(template)
+	f = None
+	try:
+		f = open(template, "w")
+		f.write(tmpl.render(config = config))
+	except Exception, e:
+		print >>sys.stderr, e
+		sys.exit(1)
+	finally:
+		if f!=None: f.close
+
+def load_template(template):
+	from mako.template import Template
+	return Template(filename=template, output_encoding='utf-8', encoding_errors='replace')
+
+def __runTemplatingDescriptorTest():
+	baseDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+	baseDir = os.path.dirname(baseDir)
+	sys.path.append(os.path.join(baseDir, 'common'))
+	from mako.template import Template
+	barFile = os.path.join(template_dir, 'templates', 'bar-descriptor.xml')
+	tmpl = load_template(barFile)
+	print tmpl.render(config = bb.configDescriptor)
+	
+def __runTemplatingProjectTest():
+	baseDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+	baseDir = os.path.dirname(baseDir)
+	sys.path.append(os.path.join(baseDir, 'common'))
+	from mako.template import Template
+	projectFile = os.path.join(template_dir, 'templates', 'project')
+	tmpl = load_template(projectFile)
+	print tmpl.render(config = bb.configProject)
+
 if __name__ == '__main__':
+	# Running unit test for -t parameter
+	if len(sys.argv) == 2 and sys.argv[1]=='-t':
+		baseDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+		baseDir = os.path.dirname(baseDir)
+		sys.path.append(os.path.join(baseDir, 'common'))
+		from tiunittest import UnitTest
+		
+		bbndk = BlackberryNDK('c:\\bbndk-2.0.0')
+		bb = Blackberry('TemplateTest', 'com.macadamian.template', bbndk)
+		
+		with UnitTest('Test template replacing on bar-descriptor.xml file..'):
+			__runTemplatingDescriptorTest()
+			
+		with UnitTest('Test template replacing on .project file..'):
+			__runTemplatingProjectTest()
+		
+		sys.exit(1)
+		
 	# This script is only meant to be invoked from project.py
 	if len(sys.argv) != 5 or sys.argv[1]=='--help':
 		print "Usage: %s <name> <id> <directory> <blackberry_ndk>" % os.path.basename(sys.argv[0])
@@ -80,4 +161,4 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 	bb = Blackberry(sys.argv[1].decode("utf-8"), sys.argv[2].decode("utf-8"), bbndk)
-	bb.create(sys.argv[3]).decode("utf-8")
+	bb.create(sys.argv[3].decode("utf-8"))
