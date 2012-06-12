cd8c694c8e624000e30c5181536c3a802a8ffbc0
David Lifshitz <dlifshitz@macadamian.com>
2012-06-12 13:12:43 -0400

    TIMOB-9279: BlackBerry: Add --blackberry-device and --blackberry-password to drillbit
    [Issues Fixed]
    TIMOB-9279: BlackBerry: Add --blackberry-device and --blackberry-password to drillbit
    
    [Changes]
    drillbit.py
    - added usage
    
    blackberry.js
    - function BlackBerrySimulator
      - store the blackberry args
    - function createDeviceManagementProcess
      - replace the unused args with the used ones
    - functions isTestHarnessRunning, pushTestJS, launchTestHarness, killTestHarness
      - use the builder process instead of devicemanagement
    
    builder.py
    - functions __init__, __name__
      - add useLogFile param to direct output to log file or stdout based on the command
    - function run
      - move the package name to a reusable function getPackage
    - added functions terminateApp, isAppRunning, printExitCode, getFile, putFile, appLog
    - function __name__
      - added usage, options, and handling for the new commands
    
    devicemanagement.py
    - use optparse instead of argparse
    - move package-related commands to builder.py
    
    [Tests]
    Test 1: Run drillbit
    1) Run scons to build the mobile sdk
    2) Run the following command:
    drillbit\drillbit.py --platforms=blackberry --blackberry-device=<ip address>
    3) Click "run tests"
    4) Verify the test_harness app is built, deployed, and run
    5) Verify the logs are printed
    Note: The test will fail because some things aren't implemented

--- support/blackberry/devicemanagement.py
+++ support/blackberry/devicemanagement.py
@@ -8,7 +8,8 @@
 #
 # General script for interacting with a Blackberry device
 #
-import os, platform, subprocess, sys, argparse
+import os, platform, subprocess, sys
+from optparse import OptionParser
 
 template_dir = os.path.abspath(os.path.dirname(sys._getframe(0).f_code.co_filename))
 top_support_dir = os.path.dirname(template_dir)
@@ -21,94 +22,36 @@ from blackberryndk import BlackberryNDK
 from builder import Builder
 
 class DeviceManagement(object):
-	def __init__(self, project_dir, type, ndk):
-		self.top_dir = project_dir.rstrip(os.sep)
-		(self.variant, self.cpu) = Builder.type2variantCpu[type]
+	def __init__(self, ndk):
 		self.ndk = ndk
-		project_tiappxml = os.path.join(self.top_dir, 'tiapp.xml')
-
-		# hide property output
-		with open(os.devnull, 'w') as nul:
-			oldStdout = sys.stdout
-			sys.stdout = nul
-			tiappxml = TiAppXML(project_tiappxml, True)
-			sys.stdout = oldStdout
-		self.name = tiappxml.properties['name']
-		self.buildDir = os.path.join(self.top_dir, 'build', 'blackberry', self.name)
-
-	def getDevice(self):
-		# TODO Mac: either get this through a passed argument or try to detect
-		return '192.168.226.132'
-
-	def getPackage(self):
-		return os.path.join(self.buildDir, self.cpu, self.variant, '%s.bar' % self.name)
-
-	def terminateApp(self):
-		return self.ndk.terminateApp(self.getDevice(), self.getPackage())
-
-	def isAppRunning(self):
-		return self.ndk.isAppRunning(self.getDevice(), self.getPackage())
-
-	def printExitCode(self):
-		return self.ndk.printExitCode(self.getDevice(), self.getPackage())
-
-	def getFile(self, hostFile, deviceFile):
-		return self.ndk.getFile(self.getDevice(), self.getPackage(), hostFile, deviceFile)
-
-	def putFile(self, hostFile, deviceFile):
-		return self.ndk.putFile(self.getDevice(), self.getPackage(), hostFile, deviceFile)
-
-	def appLog(self):
-		return self.ndk.appLog(self.getDevice(), self.getPackage())
 
 if __name__ == "__main__":
 
 	# Setup script usage
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-t', '--type', choices=['simulator', 'device', 'deploy'], required=True)
-	parser.add_argument('-d', '--project_path', help='project directory path', required=True)
-	parser.add_argument('-p', '--ndk_path', help='blackberry ndk path')
+	parser = OptionParser()
 
-	subparser = parser.add_subparsers(dest='subparser_name')
-	subparser.add_parser('getDevice')
-	subparser.add_parser('terminateApp')
-	subparser.add_parser('isAppRunning')
-	subparser.add_parser('printExitCode')
-	getFileParser = subparser.add_parser('getFile')
-	getFileParser.add_argument('hostFile')
-	getFileParser.add_argument('deviceFile')
-	putFileParser = subparser.add_parser('putFile')
-	putFileParser.add_argument('hostFile')
-	putFileParser.add_argument('deviceFile')
-	subparser.add_parser('appLog')
+	commonGroup = parser.add_option_group('Common options')
+	commonGroup.add_option('--device', help = 'hostname or the IP address of the target device or simulator', required = True)
+	commonGroup.add_option('--device-password', help = 'password for device')
+	commonGroup.add_option('-p', '--ndk-path', help = 'blackberry ndk path')
 
 	# Parse input and call apropriate function
-	args = parser.parse_args()
+	(options, args) = parser.parse_args()
+
+	device = options.device and options.device.decode('utf-8')
+	devicePassword = options.device_password and options.device_password.decode('utf-8')
+	ndkPath = options.ndk_path and options.ndk_path.decode('utf-8')
 
 	log = TiLogger(None, level = TiLogger.INFO)
 	try:
-		bbndk = BlackberryNDK(args.ndk_path and args.ndk_path.decode('utf-8'), log = log)
+		bbndk = BlackberryNDK(ndkPath, log = log)
 	except Exception, e:
 		print >>sys.stderr, e
 		sys.exit(1)
-	deviceManagement = DeviceManagement(args.project_path.decode('utf-8'), args.type.decode('utf-8'), bbndk)
+	deviceManagement = DeviceManagement(bbndk)
 
 	try:
-		retCode = 0
-		if (args.subparser_name == 'getDevice'):
-			print deviceManagement.getDevice()
-		elif (args.subparser_name == 'terminateApp'):
-			retCode = deviceManagement.terminateApp()
-		elif (args.subparser_name == 'isAppRunning'):
-			retCode = deviceManagement.isAppRunning()
-		elif (args.subparser_name == 'printExitCode'):
-			retCode = deviceManagement.printExitCode()
-		elif (args.subparser_name == 'getFile'):
-			retCode = deviceManagement.getFile(args.hostFile.decode('utf-8'), args.deviceFile.decode('utf-8'))
-		elif (args.subparser_name == 'putFile'):
-			retCode = deviceManagement.putFile(args.hostFile.decode('utf-8'), args.deviceFile.decode('utf-8'))
-		elif (args.subparser_name == 'appLog'):
-			retCode = deviceManagement.appLog()
+		retCode = 1
 		sys.exit(retCode)
 	except Exception, e:
 		print >>sys.stderr, e
