0435820aa909bd6c2e57ef74fb6e4ca010fb4764
D Campbell <dcampbell@macadamian.com>
2012-08-02 11:31:23 -0400

    TIMOB-10271: Implement getZIndex on UI View Reviewer: JP
    Change Log:
    - added setZIndex and getZIndex properties to NativeControlObject
    - added zIndex to TiUIBase
    - modified tibbtest/app.js to test zindex
    
    Test Cases:
    - build tibb
    - build tibbtest
    - run tibbtest
    - a blue label at the top should display zindex of 'Button 1' and 'Button 2'
    - default value should be 'undefined'
    - push 'Push Me' button
    - button 1 should overlap button 2
    - label at top should display:
      'ZIndex of button 1=200, ZIndex of button 2=undefined'
    - push 'Push Me' button again
    - button 2 should overlap button 1
    - label at top should display:
      'ZIndex of button 1=200, ZIndex of button 2=500'
    
    https://jira.appcelerator.org/browse/TIMOB-10271

--- blackberry/tibbtest/app.js
+++ blackberry/tibbtest/app.js
@@ -15,7 +15,16 @@ var label1 = Ti.UI.createLabel({
 	top: 200
 });
 
+var label2 = Ti.UI.createLabel({
+	textAlign : 'center',
+	font: {fontSize:20},
+	color:'blue',
+	top: 0,
+	text: 'ZOrder',
+});
+
 win1.add(label1);
+win1.add(label2);
 
 var mybutton=Ti.UI.createButton
 (
@@ -28,7 +37,17 @@ mybutton.addEventListener
 	function(e)
 	{
 		e.source.title='Pushed!';
-		win1.remove(progress1);
+		if(testbutton1.zIndex===undefined)
+		{
+			testbutton1.zIndex=200;
+		}
+		else
+		{
+			if(testbutton2.zIndex===undefined)
+			{
+				testbutton2.zIndex=500;
+			}
+		}
 	}
 );
 
@@ -41,7 +60,6 @@ var slider1=Ti.UI.createSlider
 		min: 0,
 		max: 100,
 		value: 50,
-		width: '100mm',
 	}
 );
 
@@ -52,6 +70,7 @@ var sliderListener=function(e)
 	label1.top=e.value;
 	mybutton.opacity=e.value/100.0;
 	mybutton.left=e.value;
+	mybutton.top=e.value;
 };
 
 slider1.addEventListener('change',sliderListener);
@@ -73,5 +92,23 @@ var progress1=Ti.UI.createProgressBar({
 
 win1.add(progress1);
 
+var testbutton1=Ti.UI.createButton
+(
+	{top: 500,left:0,title: 'Button 1'}
+);
+win1.add(testbutton1);
+
+var testbutton2=Ti.UI.createButton
+(
+	{top: 550,left:50,title: 'Button 2'}
+);
+win1.add(testbutton2);
+
 // open window
 win1.open();
+
+var timer=setInterval(function()
+{
+	label2.text='ZIndex of button1='+testbutton1.zIndex+', Zindex of button2='+testbutton2.zIndex;
+},500
+);
