a840ba9562780819c95bc889b6403f00c3680ae2
Harut <hmovsisyan@macadamian.com>
2012-08-24 14:53:19 +0500

    TIMOB-10162: Implement Ti.Stream.pump for Anvil
    Reviewer: David L.
    
    Change Log:
    - implemented Ti.Stream.pump function in the boostram.js
    - fixed an issue with buffer.clear function
    
    Test Cases:
    - run the java script , make sure pump is reading from the buffer, and
      calling the call back function and callback's arguments values are
        correct
    - make sure the output is expected

--- blackberry/tibbtest/app.js
+++ blackberry/tibbtest/app.js
@@ -8,117 +8,26 @@ var win1 = Titanium.UI.createWindow({
     backgroundColor:'#F00'
 });
 
-var label1 = Ti.UI.createLabel({
-	textAlign : 'center',
-	font: {fontSize:12},
-	color:'green',
-	top: 200
-});
+var pumpCallback = function(e) {
+			if (e.bytesProcessed == -1) {
+				Ti.API.info("Can't perform any more operation.");
 
-var label2 = Ti.UI.createLabel({
-	textAlign : 'center',
-	font: {fontSize:20},
-	color:'blue',
-	top: 0,
-	text: 'ZIndex',
-});
-
-win1.add(label1);
-win1.add(label2);
-
-var mybutton=Ti.UI.createButton
-(
-	{top: 100,title: 'Push Me'}
-);
-
-mybutton.addEventListener
-(
-	'click',
-	function(e)
-	{
-		e.source.title='Pushed!';
-		testbutton1.zIndex=15;
-	}
-);
-
-win1.add(mybutton);
-
-var slider1=Ti.UI.createSlider
-(
-	{
-		top: 200,
-		min: 0,
-		max: 100,
-		value: 50,
-	}
-);
-
-var sliderListener=function(e)
-{
-	progress1.value=100-e.value;
-	label1.text='Slider value: '+e.value;
-	label1.top=e.value;
-	mybutton.opacity=e.value/100.0;
-	mybutton.left=e.value;
-	mybutton.top=e.value;
+			} else if (e.errorDescription == null || e.errorDescription == "") {
+				var data = e.buffer.toString();
+				Ti.API.info("Buffer :" + data);
+				Ti.API.info("TotalBytesProcessed :" + e.totalBytesProcessed);
+				Ti.API.info("bytesProcessed :" + e.bytesProcessed);
+ 
+			} else {
+				Ti.API.info("READ ERROR: " + e.errorDescription);
+            }
 };
+ 
+var buffer = Ti.createBuffer({ value: "Some long sentence for testing purpose, blah blah blah." });
+var stream = Ti.Stream.createStream({ source: buffer });
 
-slider1.addEventListener('change',sliderListener);
-label1.addEventListener('click',function(e)
-	{
-		label1.text='Label was clicked';
-	}
-	);
+Ti.Stream.pump(stream, pumpCallback, 7, true);
 
-
-win1.add(slider1);
-
-var progress1=Ti.UI.createProgressBar({
-	value:0,
-	min: 0,
-	max: 100,
-	top: 300,
-});
-
-win1.add(progress1);
-
-var testbutton1=Ti.UI.createButton
-(
-	{top: 500,left:0,title: 'Button 1',zIndex:0}
-);
-win1.add(testbutton1);
-
-var testbutton2=Ti.UI.createButton
-(
-	{top: 550,left:50,title: 'Button 2',zIndex:20}
-);
-win1.add(testbutton2);
-
-var testbutton3=Ti.UI.createButton
-(
-	{top: 600,left:100,title: 'Button 3',zIndex:25}
-);
-win1.add(testbutton3);
-
-var testbutton4=Ti.UI.createButton
-(
-	{top: 650,left:150,title: 'Button 4',zIndex:10}
-);
-win1.add(testbutton4);
 
 // open window
 win1.open();
-
-var timer=setInterval(function()
-{
-	label2.text='ZIndex of button1='+testbutton1.zIndex+', Zindex of button2='+testbutton2.zIndex;
-	testbutton1.top=500+Math.floor(Math.random()*500);
-	testbutton1.left=Math.floor(Math.random()*500);
-	testbutton2.top=500+Math.floor(Math.random()*500);
-	testbutton2.left=Math.floor(Math.random()*500);
-	testbutton3.top=500+Math.floor(Math.random()*500);
-	testbutton3.left=Math.floor(Math.random()*500);
-	testbutton4.top=500+Math.floor(Math.random()*500);
-	testbutton4.left=Math.floor(Math.random()*500);
-},5000
-);
