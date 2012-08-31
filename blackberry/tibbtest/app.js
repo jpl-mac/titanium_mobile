ecd237e97e80f4149930735e025ef5df194a1fc9
Harut <hmovsisyan@macadamian.com>
2012-08-31 17:39:39 +0500

    addressed comments and re-run test

--- blackberry/tibbtest/app.js
+++ blackberry/tibbtest/app.js
@@ -1,124 +1,60 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-//Titanium.UI.setBackgroundColor('#000');
-
-//
-// create root window
-//
-var win1 = Titanium.UI.createWindow({  
-    backgroundColor:'#F00'
+var win1 = Titanium.UI.createWindow({
+backgroundColor:'#F00'
 });
 
-var label1 = Ti.UI.createLabel({
-	textAlign : 'center',
-	font: {fontSize:12},
-	color:'green',
-	top: 200
-});
+var pumpCallback = function(e) {
+			if (e.bytesProcessed == -1) { // EOF
+				Ti.API.info("<EOF> - Can't perform any more operations on connected socket");
 
-var label2 = Ti.UI.createLabel({
-	textAlign : 'center',
-	font: {fontSize:20},
-	color:'blue',
-	top: 0,
-	text: 'ZIndex',
-});
+			} else if (e.errorDescription == null || e.errorDescription == "") {
+				var data = e.buffer.toString();
+				Ti.API.info("DATA!!!: " + data);
 
-win1.add(label1);
-win1.add(label2);
-
-var mybutton=Ti.UI.createButton
-(
-	{top: 100,title: 'Push Me'}
-);
-
-mybutton.addEventListener
-(
-	'click',
-	function(e)
-	{
-		e.source.title='Pushed!';
-		testbutton1.zIndex=15;
-	}
-);
-
-win1.add(mybutton);
-
-var slider1=Ti.UI.createSlider
-(
-	{
-		top: 200,
-		min: 0,
-		max: 100,
-		value: 50,
-	}
-);
-
-var sliderListener=function(e)
-{
-	progress1.value=100-e.value;
-	label1.text='Slider value: '+e.value;
-	label1.top=e.value;
-	mybutton.opacity=e.value/100.0;
-	mybutton.left=e.value;
-	mybutton.top=e.value;
+			} else {
+				Ti.API.info("READ ERROR: " + e.errorDescription);
+			}
 };
 
-slider1.addEventListener('change',sliderListener);
-label1.addEventListener('click',function(e)
-	{
-		label1.text='Label was clicked';
+var driverSocket;
+
+var listenSocket = Ti.Network.Socket.createTCP({
+    host: '192.168.163.130',
+    port: 40404,
+
+    accepted: function(e) {
+    Ti.API.info("ACCEPTTED callback.");
+    driverSocket = e.inbound;
+	var readyMessage = {type: "ready"};
+    driverSocket.write(Ti.createBuffer({value: JSON.stringify(readyMessage)}));
+
+    Ti.Stream.pump(driverSocket, pumpCallback, 1024, true);
+		},
+	error: function(e) {
+	Ti.API.info("ERROR callback asdf.");
+		//e.socket.close();
 	}
-	);
-
-
-win1.add(slider1);
-
-var progress1=Ti.UI.createProgressBar({
-	value:0,
-	min: 0,
-	max: 100,
-	top: 300,
 });
 
-win1.add(progress1);
+listenSocket.listen();
 
-var testbutton1=Ti.UI.createButton
-(
-	{top: 500,left:0,title: 'Button 1',zIndex:0}
-);
-win1.add(testbutton1);
+listenSocket.accept({
 
-var testbutton2=Ti.UI.createButton
-(
-	{top: 550,left:50,title: 'Button 2',zIndex:20}
-);
-win1.add(testbutton2);
+});
 
-var testbutton3=Ti.UI.createButton
-(
-	{top: 600,left:100,title: 'Button 3',zIndex:25}
-);
-win1.add(testbutton3);
+var button = Titanium.UI.createButton({
+title: 'Stop',
+top: 10,
+width: 200,
+height: 100
+});
 
-var testbutton4=Ti.UI.createButton
-(
-	{top: 650,left:150,title: 'Button 4',zIndex:10}
-);
-win1.add(testbutton4);
+button.addEventListener('click',function(e)
+{
+var testMsg = "Message From Server";
+driverSocket.write(Ti.createBuffer({value: JSON.stringify(testMsg)}));
+});
+
+win1.add(button);
 
 // open window
 win1.open();
-
-var timer=setInterval(function()
-{
-	label2.text='ZIndex of button1='+testbutton1.zIndex+', Zindex of button2='+testbutton2.zIndex;
-	testbutton1.top=500+Math.floor(Math.random()*500);
-	testbutton1.left=Math.floor(Math.random()*500);
-	testbutton2.top=500+Math.floor(Math.random()*500);
-	testbutton2.left=Math.floor(Math.random()*500);
-	testbutton3.top=500+Math.floor(Math.random()*500);
-	testbutton3.left=Math.floor(Math.random()*500);
-	testbutton4.top=500+Math.floor(Math.random()*500);
-	testbutton4.left=Math.floor(Math.random()*500);
-},5000
-);
